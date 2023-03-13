b86765b7a18292058cc07830198a2138f09b3440
hiromitsu-yokoyama <hyokoyama@i.gmobb.jp>
2023-03-13 20:38:47 +0900

    third

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import time
 from selenium import webdriver
 
-driver = webdriver.Edge(executable_path="msedgedriver.exe")
+driver = webdriver.Chrome('venv/chromedriver')
 driver.get("https://www.yahoo.co.jp/")
 time.sleep(5)
 driver.quit()
+
+
+
+
